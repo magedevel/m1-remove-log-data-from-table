aa41c05e7c3bbcf0decb53834f51b516b70b1b02
Magedevel.com <20953761+magedevel@users.noreply.github.com>
2021-01-13 14:37:45 +0400

    Create delete-log.sql

---
+++ delete-log.sql
@@ -0,0 +1,17 @@
+SET FOREIGN_KEY_CHECKS=0;
+TRUNCATE dataflow_batch_export;
+TRUNCATE dataflow_batch_import;
+TRUNCATE log_customer;
+TRUNCATE log_quote;
+TRUNCATE log_summary;
+TRUNCATE log_summary_type;
+TRUNCATE log_url;
+TRUNCATE log_url_info;
+TRUNCATE log_visitor;
+TRUNCATE log_visitor_info;
+TRUNCATE log_visitor_online;
+TRUNCATE report_viewed_product_index;
+TRUNCATE report_compared_product_index;
+TRUNCATE report_event;
+TRUNCATE index_event;
+SET FOREIGN_KEY_CHECKS=1;
